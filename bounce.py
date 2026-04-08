513449d8fbfc4d7b240cce01fea2f1712c2d3e15
THOMAS MIGADDE <thomasmigadde@gmail.com>
2026-04-08 15:51:04 +0300

    update

--- bounce.py
+++ bounce.py
@@ -14,5 +14,5 @@ for obj in [cube1, cube2]:
     obj.rigid_body.collision_shape = 'BOX'
     obj.rigid_body.restitution = 0.8 # Bounciness (0 = no bounce, 1 = very bouncy)
 
-# Optional: give cube1 some initial velocity
+# u can: give cube1 some initial velocity
 cube1.rigid_body.linear_velocity = (5, 0, 0)
